f63c850232035bfa9a7d85e3fcce11e608ddc403
Ryan Tierney <Ryan.Tierney92@gmail.com>
2020-08-05 20:57:57 -0500

    stubbed out models and updated controllers with empty actions

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,14 @@
 class RentalsController < ApplicationController
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def show 
+    end 
+
+    def index 
+    end 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord 
+    has_many :rentals 
+    has_many :movies, through: :rentals 
+
+end 
